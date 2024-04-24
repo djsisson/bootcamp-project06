befd9f149d06fcf1473179b7fd17a61b7cfc2023
Darren Sisson <darrensisson@hotmail.com>
2024-04-24 13:24:24 +0100

    added rnd position

--- src/Components/VisualScore/VisualScore.jsx
+++ src/Components/VisualScore/VisualScore.jsx
@@ -1,8 +1,16 @@
+import {useEffect, useState} from "react"
 import "./VisualScore.css"
 
 const VisualScore = ({className, value}) => {
+const [rndLeft, setRndLeft] = useState(0)
+const [rndTop, setRndTop] = useState(0)
 
-    return (<div className={className}>+{value}</div>)
+useEffect(() => {
+    setRndLeft(Math.floor(Math.random() * 40 - 20))
+    setRndTop(Math.floor(Math.random() * 40 - 20))
+}, [])
+
+    return (<div className={className} style={{"--rndLeft": `${rndLeft}px`, "--rndTop": `${rndTop}px`}}>+{value}</div>)
 }
 
 export default VisualScore
